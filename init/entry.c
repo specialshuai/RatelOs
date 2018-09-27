6b91c0b5179bfb2dcce7500a4a30b12215d4c5e8
heshuai <13813327632@163.com>
2018-09-27 20:00:05 +0800

    4th commit

---
+++ init/entry.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  entry.c
+ *
+ *    Description:  RatelOs 内核的入口C函数
+ *
+ * =====================================================================================
+ */
+ 
+#include "type.h"
+ 
+int kern_entry()
+{
+	uint8_t *input = (uint8_t *)0xB8000;
+	uint8_t color = (0 << 4) | (15 & 0x0F);
+	
+	*input++ = 'H'; *input++ = color;
+	*input++ = 'e'; *input++ = color;
+	*input++ = 'l'; *input++ = color;
+	*input++ = 'l'; *input++ = color;
+	*input++ = 'o'; *input++ = color;
+	*input++ = ' '; *input++ = color;
+	*input++ = 'R'; *input++ = color;
+	*input++ = 'a'; *input++ = color;
+	*input++ = 't'; *input++ = color;
+	*input++ = 'e'; *input++ = color;
+	*input++ = 'l'; *input++ = color;	
+	*input++ = 'O'; *input++ = color;
+	*input++ = 's'; *input++ = color;
+	*input++ = ' '; *input++ = color;
+	*input++ = 'K'; *input++ = color;
+	*input++ = 'e'; *input++ = color;
+	*input++ = 'r'; *input++ = color;
+	*input++ = 'n'; *input++ = color;
+	*input++ = 'e'; *input++ = color;
+	*input++ = 'l'; *input++ = color;
+	*input++ = '!'; *input++ = color;
+	
+	return 0;
+}
