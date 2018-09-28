040b0692d8d1f5ff1836d4202238c5e17f02183d
heshuai <13813327632@163.com>
2018-09-28 15:52:06 +0800

    add console_func

--- init/entry.c
+++ init/entry.c
@@ -8,34 +8,13 @@
  * =====================================================================================
  */
  
-#include "type.h"
+#include "console.h"
  
 int kern_entry()
 {
-	uint8_t *input = (uint8_t *)0xB8000;
-	uint8_t color = (0 << 4) | (15 & 0x0F);
-	
-	*input++ = 'H'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'o'; *input++ = color;
-	*input++ = ' '; *input++ = color;
-	*input++ = 'R'; *input++ = color;
-	*input++ = 'a'; *input++ = color;
-	*input++ = 't'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'l'; *input++ = color;	
-	*input++ = 'O'; *input++ = color;
-	*input++ = 's'; *input++ = color;
-	*input++ = ' '; *input++ = color;
-	*input++ = 'K'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'r'; *input++ = color;
-	*input++ = 'n'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = '!'; *input++ = color;
+	console_clear();
+
+	console_write_color("Hello, RatelOs kernel!\n", rc_black, rc_green);
 	
 	return 0;
 }
