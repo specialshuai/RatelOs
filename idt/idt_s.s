c02c6a69fbb23d3407d5ae1d2762268578c169ac
shuai <specialshuai@163.com>
2018-10-24 16:17:37 +0800

    add IDT

---
+++ idt/idt_s.s
@@ -0,0 +1,118 @@
+; --------------------------------------------------
+;
+; 	将 IDT 地址 载入 IDTR
+;	这段代码是我们常用的中断处理程序。它首先使用'pusha'命令来推送堆栈上的所有通用寄存器。它使用'popa'命令在最后恢复它们。
+;	它还获取当前数据段选择器并将其推送到堆栈，将所有段寄存器设置为内核数据选择器，然后再恢复它们。这实际上不会产生影响，
+;	但是当我们切换到用户模式时它会。请注意，它还调用更高级别的中断处理程序 - isr_handler。
+;	当中断触发时，处理器会自动将有关处理器状态的信息压入堆栈。按代码段，指令指针，标志寄存器，堆栈段和堆栈指针。
+;	IRET指令专门用于从中断返回。它将这些值从堆栈中弹出并将处理器返回到原来的状态。
+;
+;---------------------------------------------------
+
+[GLOBAL idt_flush]    ; 允许C代码调用idt_flush（）
+idt_flush:
+	mov eax, [esp+4]  ; 参数存入 eax 寄存器
+	lidt [eax]        ; 加载到 IDTR
+	ret
+.end:
+
+;[ GLOBAL  isr0] 
+;isr0：
+;  cli                  ; 禁止中断
+;  push byte 0          ; 推送一个虚拟错误代码（如果ISR0没有推送它自己的错误代码）
+;  push byte 0          ; 推中断号（0）
+;  jmp  isr_common_stub  ; 转到我们的公共处理程序。
+;  32个版本仍然听起来像很多代码, 使用NASM的宏来减少这种情况
+
+ 
+; 定义两个构造中断处理函数的宏(有的中断有错误代码，有的没有)
+; 用于没有错误代码的中断
+%macro ISR_NOERRCODE 1			; 定义一个宏，取一个参数
+[GLOBAL isr%1]                  ; 允许C代码调用isr0-255（）
+isr%1:							; ％1访问第一个参数
+	cli                         ; 首先关闭中断
+	push 0                      ; push 无效的中断错误代码(起到占位作用，便于所有isr函数统一清栈)
+	push %1                     ; push 中断号
+	jmp isr_common_stub
+%endmacro
+
+; 用于有错误代码的中断
+%macro ISR_ERRCODE 1
+[GLOBAL isr%1]
+isr%1:
+	cli                         ; 关闭中断
+	push %1                     ; push 中断号
+	jmp isr_common_stub
+%endmacro
+
+; make a handler just doing
+; 定义中断处理函数
+
+ISR_NOERRCODE  0 	; 0 #DE 除 0 异常
+ISR_NOERRCODE  1 	; 1 #DB 调试异常
+ISR_NOERRCODE  2 	; 2 NMI
+ISR_NOERRCODE  3 	; 3 BP 断点异常 
+ISR_NOERRCODE  4 	; 4 #OF 溢出 
+ISR_NOERRCODE  5 	; 5 #BR 对数组的引用超出边界 
+ISR_NOERRCODE  6 	; 6 #UD 无效或未定义的操作码 
+ISR_NOERRCODE  7 	; 7 #NM 设备不可用(无数学协处理器) 
+ISR_ERRCODE    8 	; 8 #DF 双重故障(有错误代码) 
+ISR_NOERRCODE  9 	; 9 协处理器跨段操作
+ISR_ERRCODE   10 	; 10 #TS 无效TSS(有错误代码) 
+ISR_ERRCODE   11 	; 11 #NP 段不存在(有错误代码) 
+ISR_ERRCODE   12 	; 12 #SS 栈错误(有错误代码) 
+ISR_ERRCODE   13 	; 13 #GP 常规保护(有错误代码) 
+ISR_ERRCODE   14 	; 14 #PF 页故障(有错误代码) 
+ISR_NOERRCODE 15 	; 15 CPU 保留 
+ISR_NOERRCODE 16 	; 16 #MF 浮点处理单元错误 
+ISR_ERRCODE   17 	; 17 #AC 对齐检查 
+ISR_NOERRCODE 18 	; 18 #MC 机器检查 
+ISR_NOERRCODE 19 	; 19 #XM SIMD(单指令多数据)浮点异常
+
+; 20~31 Intel 保留
+ISR_NOERRCODE 20
+ISR_NOERRCODE 21
+ISR_NOERRCODE 22
+ISR_NOERRCODE 23
+ISR_NOERRCODE 24
+ISR_NOERRCODE 25
+ISR_NOERRCODE 26
+ISR_NOERRCODE 27
+ISR_NOERRCODE 28
+ISR_NOERRCODE 29
+ISR_NOERRCODE 30
+ISR_NOERRCODE 31
+; 32～255 用户自定义
+ISR_NOERRCODE 255
+
+[GLOBAL isr_common_stub]
+[EXTERN isr_handler]
+; 中断服务程序
+isr_common_stub:
+	pusha                    ; Pushes edi, esi, ebp, esp, ebx, edx, ecx, eax
+	mov ax, ds
+	push eax                ; 保存数据段描述符
+	
+	mov ax, 0x10            ; 加载内核数据段描述符表
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	
+	push esp		; 此时的 esp 寄存器的值等价于 pt_regs 结构体的指针
+	call isr_handler        ; 在 C 语言代码里
+	add esp, 4 		; 清除压入的参数
+	
+	pop ebx                 ; 恢复原来的数据段描述符
+	mov ds, bx
+	mov es, bx
+	mov fs, bx
+	mov gs, bx
+	mov ss, bx
+	
+	popa                     ; Pops edi, esi, ebp, esp, ebx, edx, ecx, eax
+	add esp, 8               ; 清理栈里的 error code 和 ISR
+	iret
+.end:
+
--- init/entry.c
+++ init/entry.c
@@ -12,14 +12,20 @@
 #include "gdt.h"
 #include "console.h"
 #include "debug.h"
+#include "idt.h"
 
 int kern_entry()
 {
 	init_debug();
 	init_gdt();
-	
+	init_idt();
+
 	console_clear();
 	printk_color(rc_black, rc_green, "Hello, RatelOs kernel!\n");
+	
+	asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
+	asm volatile ("int $0x5");
 	
 	return 0;
 }
